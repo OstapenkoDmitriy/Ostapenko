114022caa03fc31f496dda8b043c35aba05e3172
OstapenkoDmitriy <ostapenkodima1991@gmail.com>
2021-09-01 23:46:43 +0300

    Update sent_form.php

--- public/sent_form.php
+++ public/sent_form.php
@@ -10,10 +10,13 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-
+$name = htmlspecialchars($_POST["user_name"]);
+$name_org = htmlspecialchars($_POST["name_org"]);
+$user_mail = htmlspecialchars($_POST["user_mail"]);
+$user_message = htmlspecialchars($_POST["user_message"]);
+$amount = htmlspecialchars($_POST["amount"]);
 $sql = "INSERT INTO invite_to_speeach (name, organization, email, message, amount, date_of_create, id)
-VALUES (htmlspecialchars($_POST["user_name"]), htmlspecialchars($_POST["name_org"]),
- htmlspecialchars($_POST["user_mail"]), htmlspecialchars($_POST["user_message"]), htmlspecialchars($_POST["amount"]),
+VALUES ($name, $name_org, $user_mail, $user_message, $amount,
 date('Y-m-d'), 0)";
 
 if ($conn->query($sql) === TRUE) {
