31cb406be91f964921f4151193489a19304cf49c
OstapenkoDmitriy <ostapenkodima1991@gmail.com>
2021-09-22 22:17:55 +0300

    form

--- public/sent_form.php
+++ public/sent_form.php
@@ -15,11 +15,11 @@ $name_org = htmlspecialchars($_POST["name_org"]);
 $user_mail = htmlspecialchars($_POST["user_mail"]);
 $user_message = htmlspecialchars($_POST["user_message"]);
 $amount = htmlspecialchars($_POST["amount"]);
+$date = date('Y-m-d');
 $sql = "INSERT INTO invite_to_speeach (name, organization, email, message, amount, date_of_create, id)
+VALUES ('$name', '$name_org', '$user_mail', '$user_message', '$amount',
+'$date', 0)";
 $result = $conn->query($sql);
-VALUES ($name, $name_org, $user_mail, $user_message, $amount,
-date('Y-m-d'), 0)";
-
 if (!$result) {
   echo "ошибка";
 } else {
